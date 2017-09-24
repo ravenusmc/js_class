31a28f621d3a93c62d99c7eb017f48a6ce49f65f
ravenusmc <mcuddy77@gmail.com>
2017-09-24 13:40:25 -0400

    minor changes

--- chap/form.js
+++ chap/form.js
@@ -3,7 +3,7 @@
 //Global variables
 var twentyNine = document.createDocumentFragment();
 var thirty = document.createDocumentFragment();
-var thiryOne = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
 
 function setupDays() {
   var dates = document.getElementById('delivDy').getElementsByTagName('option');
@@ -37,9 +37,12 @@ function updateDays(){
     deliveryDay.appendChild(twentyNine.cloneNode(true));
   }else if (selectedMonth === '4' || selectedMonth === '6' || selectedMonth === '9' || selectedMonth === '11'){
     deliveryDay.appendChild(thirty.cloneNode(true));
-  }else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth ==== "7" || selectedMonth)
+  }else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || 
+    selectedMonth === "10" || selectedMonth === "12"){
+    deliveryDay.appendChild(thirtyOne.cloneNode(true));
+  }
 
-}
+};
 
 function removeSelectDefaults() {
   var emptyBoxes = document.getElementsByTagName("select");
@@ -47,10 +50,47 @@ function removeSelectDefaults() {
   for (var i = 0; i < emptyBoxes.length; i++){
     emptyBoxes[i].selectedIndex = -1;
   }
+};
+
+function autocheckCustom() {
+  var messageBox = document.getElementById('customText');
+
+  if (messageBox.vale !== "" && messageBox.value !== messageBox.placeholder){
+    document.getElementById('custom').checked = "checked";
+  }
+}
+
+function createEventListeners() {
+  var deliveryMonth = document.getElementById("delivMo");
+  if (deliveryMonth.addEventListener){
+    deliveryMonth.addEventListener("change", updateDays, false);
+  }else if (deliveryMonth.attachEvent){
+    deliveryMonth.attachEvent('onchange', updateDays);
+  }
+
+  var deliveryYear = document.getElementById('delivYr');
+  if (deliveryYear.addEventListener){
+    deliveryYear.addEventListener('Change', updateDays, false)
+  }else if (deliveryYear.attachEvent){
+    deliveryYear.attachEvent("onchange", updateDays);
+  }
+
+  var messageBox = document.getElementById('customText');
+  if (messageBox.addEventListener){
+    messageBox.addEventListener('blur', autocheckCustom, false)
+  }else if (messageBox.attachEvent){
+    messageBox.attachEvent('onblur', autocheckCustom);
+  }
+}
+
+function setUpPage() {
+  removeSelectDefaults();
+  setupDays();
+  createEventListeners();
 }
 
 if (window.addEventListener) {
-  window.addEventListener("load", removeSelectDefaults, false);
+  window.addEventListener("load", setUpPage, false);
 }else if (window.attachEvent){
-  window.attachEvent("onload", removeSelectDefaults);
+  window.attachEvent("onload", setUpPage);
 }
