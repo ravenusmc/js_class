cdfd9ba1759df9ba069dc8ba6bda2f52f5d7867c
ravenusmc <mcuddy77@gmail.com>
2017-09-26 14:15:12 -0400

    finished work for first chapter

---
+++ 1520/ch06/6-1/script.js
@@ -0,0 +1,108 @@
+// JavaScript 6th Edition
+// Chapter 6
+// Hands-on Project 6-1
+
+// Author: Mike Cuddy
+// Date:   9/26/2017
+
+// Filename: index.html
+
+'use strict';
+
+var formValidity = true;
+
+// validate required fields 
+function validateRequired(){
+  var inputElements = document.querySelectorAll("#contactinfo input");
+  var errorDiv = document.getElementById('errorText');
+  var elementCount = inputElements.length;
+  var requiredValidity = true;
+  var currentElement;
+
+  try {
+    for (var i = 0; i < elementCount; i++){
+      //validate all input elements in fieldset
+      currentElement = inputElements[i];
+      if (currentElement.value === ""){
+        currentElement.style.background = "rgb(255,233,233)";
+        requiredValidity = false;
+      }else {
+        currentElement.style.background = 'white';
+      }
+    }
+    if (requiredValidity === false){
+      throw "Please complete all fields.";
+    }
+    errorDiv.style.display = 'none';
+    errorDiv.innerHTML = "";
+  }
+  catch (msg){
+    errorDiv.style.display = "block";
+    errorDiv.innerHTML = msg;
+    formValidity = false;
+  }
+}
+
+//Create an event listener 
+function createEventListeners() {
+  var form = document.getElementsByTagName('form')[0];
+  if (form.addEventListener){
+    form.addEventListener("submit", validateForm, false);
+  }else if (form.attachEvent) {
+    form.attachEvent("onsubmit", validateForm);
+  }
+}
+
+//Validate form
+function validateForm(evt){
+  if (evt.preventDefault){
+    evt.preventDefault(); //Prevent form from submitting
+  } else {
+    evt.returnValue = false;
+  }
+  formValidity = true; //Reset value for revalidation
+  validateRequired();
+  validateNumbers();
+  if (formValidity === true){
+    document.getElementsByTagName("form")[0].submit();
+  }
+}
+
+//Validate number fields for older browsers 
+function validateNumbers(){
+  var numberInputs = document.querySelectorAll("#contactinfo input[type=number]");
+  var elementCount = numberInputs.length;
+  var numErrorDiv = document.getElementById("numErrorText");
+  var numbersValidity = true;
+  var currentElement;
+
+  try {
+    for (var i = 0; i < elementCount; i++){
+      //validate all input types
+      currentElement = numberInputs[i];
+      if (isNaN(currentElement.value) || (currentElement.value === "")){
+        currentElement.style.background = 'rgb(255,233,233';
+        numbersValidity = false;
+      }else {
+        currentElement.style.background = "white";
+      }
+    }
+    if (numbersValidity === false){
+      throw "Zip and social security values must be numbers";
+    }
+    numErrorDiv.style.display = 'none';
+    numErrorDiv.innerHTML = "";
+  }
+  catch(msg){
+    numErrorDiv.style.display = "block";
+    numErrorDiv.innerHTML = msg;
+    formValidity = false;
+  }
+}
+
+//Run setup functions when page finishes loading 
+if (window.addEventListener){
+  window.addEventListener("load", createEventListeners, false);
+}else if (window.attachEvent){
+  window.attachEvent("onload", createEventListeners);
+}
---
+++ 1520/ch06/index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mike Cuddy</title>
+        <meta name="robots" content="noindex, nofollow">
+        <meta name="author" content="Mike Cuddy" />
+        <link rel="stylesheet" type="text/css" href="../ch01/chap.css">
+    </head>
+
+<body>
+
+    <header>
+        <h1 class='font'>Mike Cuddy</h1>
+        <h2 class='font'>CIST 1520 Chapter Six Work</h2>
+        <ul>
+          <li><a href="6-1/index.html">6-1</a></li>
+          <li><a href="6-3/index.html">6-3</a></li>
+          <li><a href="6-5/index.html">6-5</a></li>
+        </ul>
+    </header>
+
+<main class='main_area'>
+
+</main>
+
+</body>
+</html>
