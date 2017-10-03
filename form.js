2c2b3d8f8786df0cba3948df233c0cb48673df9a
ravenusmc <mcuddy77@gmail.com>
2017-10-03 19:51:06 -0400

    finished first assignment

---
+++ form.js
@@ -0,0 +1,18 @@
+// function month(){
+//   var month = document.getElementById('month').value;
+//   console.log(month);
+// }
+
+
+function info(){
+  //first_Name = document.getElementById('f_name').value;
+  // document.getElementById('address_fName').innerHTML = 'first_Name';
+  //document.getElementById('address_fName').innerHTML = document.getElementById('f_name').value;
+  //document.getElementById('address_fName').innerHTML = 'Mike'
+
+  var first_Name = document.getElementById('f_name').value;
+  var last_Name = document.getElementById('l_name').value;
+  var add_first = document.getElementById('address_fName');
+  var add_last = document.getElementById('')
+  add_first.value = first_Name;
+}
