95f74030587e9b1b2dbfb4c1a9c1a1990b2fc7d8
ravenusmc <mcuddy77@gmail.com>
2017-10-10 14:05:26 -0400

    finished 8-3 work

---
+++ 1520/ch08/8-3/script.js
@@ -0,0 +1,52 @@
+//Hands-on Project 8-3
+//Mike Cuddy
+//10-10-2017
+//script.js
+
+
+////// NOTE, PLEASE SEE THE CARDS.TXT FILE FOR THE CARD TPYES FOR PAGE 611 OF THE TEXT!!! ////////
+
+'use strict';
+
+function selectCardType() {
+
+  var cardNumValue = document.getElementById('ccNum').value;
+
+  var visa = /^4[0-9]{12}(?:[0-9]{3})?$/
+  var mc = /^5[1-5][0-9]{14}$/
+  var discover = /^6(?:011|5[0-9]{2})[0-9]{12}$/
+  var amex = /^3[47][0-9]{13}$/
+
+  if (visa.test(cardNumValue)){
+    document.getElementById("visa").checked = "checked";
+  }else if (mc.test(cardNumValue)) {
+    document.getElementById('mc').checked = 'checked';
+  }else if (discover.test(cardNumValue)){
+    document.getElementById('discover').checked = 'checked';
+  }else if (amex.test(cardNumValue)){
+    document.getElementById('amex').checked = 'checked';
+  }
+}
+
+function createEventListener() {
+
+
+  var cardNum = document.getElementById('ccNum');
+
+  if (cardNum.addEventListener) {
+    cardNum.addEventListener("change", selectCardType, false);
+  } else if (cardNum.attachEvent){
+    cardNum.attachEvent("onchange", selectCardType);
+  }
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", createEventListener, false);
+}else if (window.attachEvent){
+  window.attachEvent('onload',createEventListener);
+}
+
+
+
+
+
