e9058aaeab5b9783ed8d0b49f3043d6f5873bea7
ravenusmc <mcuddy77@gmail.com>
2017-10-14 15:50:49 -0400

    added validation function

--- 1520/midterm/index.js
+++ 1520/midterm/index.js
@@ -3,57 +3,49 @@
 //Global Variables 
 numbers = []; //This array will hold the numbers. 
 
+//This funcion will ensure that the users enter a number. 
+function validation(value, numbers, num){
+  if (isNaN(value)){
+    alert('Please enter a number on line ' + num);
+  }else {
+    numbers.push(value);
+  }
+}
+
 //This function is what will get the numbers from the input, add them together and then 
 //display them on the page. 
 function addNumbers() {
 
   //Here I am getting the numbers from the user input and placing each number into an array.
   one = Number(document.getElementById('num_one').value);
-  numbers.push(one);
+  num = 1;
+  validation(one, numbers, num);
   two = Number(document.getElementById('num_two').value);
-  numbers.push(two);
+  num = 2;
+  validation(two, numbers, num);
   three = Number(document.getElementById('num_three').value);
-  numbers.push(three);
+  num = 3;
+  validation(three, numbers, num);
   four = Number(document.getElementById('num_four').value);
-  numbers.push(four);
+  num = 4;
+  validation(four, numbers, num);
   five = Number(document.getElementById('num_five').value);
-  numbers.push(five);
+  num = 5;
+  validation(five, numbers, num);
 
   //This variable will hold the total of all the elements in the array. 
   var total = 0;
 
   //Using a for loop to sum up the numbers in the array and getting the total
-  //debugger;
   for (var i = 0; i < numbers.length; i++){
     total = numbers[i] + total;
   }
 
+  //Getting the id for where the total will be displayed.
   answer = document.getElementById('answer')
-
+  //Setting the total to the innerHTML
   answer.innerHTML = total;
 
 }
 
 
-
-// document.querySelector("#submit").addEventListener('click', addNumbers);
-
-//This is the event listener
-//Event listener for the button. 
-// function createEventListener() {
-
-//   var addButton = document.getElementById('button');
-//   if (addButton.addEventListener){
-//     addButton.addEventListener("click", addNumbers, false);
-//   } else if (addButton.attachEvent) {
-//     addButton.attachEvent("onclick", addNumbers);
-//   }
-// }
-
-
-// //This code is executed when the page loads.
-// if (window.addEventListener) {
-//   window.addEventListener("load", createEventListener, false);
-// }else if (window.attachEvent){
-//   window.attachEvent("onload", createEventListener);
-// }
