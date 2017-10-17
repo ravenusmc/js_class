8f66a84ac8c8a1d8bda31b8b0162367f690e1c29
ravenusmc <mcuddy77@gmail.com>
2017-10-17 13:52:30 -0400

    working on 9-3

---
+++ 1520/ch09/9-3/script.js
@@ -0,0 +1,59 @@
+// Hands-on Project 9-3
+// Mike Cuddy
+// 10/16/2017
+
+//PLEASE NOTE-THIS CODE WORKS IN FIREFOX BUT NOT SURE IF IT WAS WORKING IN CHROME. 
+//https://stackoverflow.com/questions/15385641/javascript-code-for-cookie-not-working-in-chrome
+
+'use strict';
+
+function processCookie() {
+  if (document.getElementById("rememberinput").checked) {
+    document.cookie = "username=" + document.getElementById("usernameinput").value;
+  }else {
+    var expiresDate = new Date();
+    console.log(expiresDate);
+    expiresDate.setDate(expiresDate.getDate() -7);
+    document.cookie = "username=null; expires=" + expiresDate.toUTCString();
+  }
+  
+}
+
+function populateInfo() {
+  if (document.cookie){
+    var uname = document.cookie;
+    uname = uname.substring(uname.lastIndexOf("=") +1);
+    document.getElementById("usernameinput").value = uname;
+  }
+}
+
+function handleSubmit(evt) {
+  if (evt.preventDefault) {
+    evt.preventDefault();
+  }else {
+    evt.returnValue = false;
+  }
+  debugger;
+  processCookie();
+  document.getElementsByTagName('form')[0].submit();
+}
+
+function createEventListener(){
+  var loginForm = document.getElementsByTagName("form")[0];
+  if (loginForm.addEventListener){
+    loginForm.addEventListener("submit", handleSubmit, false);
+  }else if (loginForm.attachEvent){
+    loginForm.attachEvent("onsubmit", handleSubmit);
+  }
+}
+
+function setUpPage(){
+  populateInfo();
+  createEventListener();
+}
+
+if (window.addEventListener){
+  window.addEventListener("load", setUpPage, false);
+}else if (window.attachEvent){
+  window.attachEvent("onload", setUpPage);
+}
