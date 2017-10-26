d3af69b72fafa3f7ba09c9fbf4de995ea4a54a0c
ravenusmc <mcuddy77@gmail.com>
2017-10-25 20:33:24 -0400

    completed 10-3

--- 1520/ch10/10-3/script.js
+++ 1520/ch10/10-3/script.js
@@ -2,11 +2,38 @@
     Chapter 10
     Hands-on Project 10-3
 
-    Author: 
-    Date:   
+    Author: Mike Cuddy
+    Date:   10/25/2017
 
     Filename: script.js
 */
 
 "use strict";
 
+var waitForUser;
+
+function geoTest() {
+
+  waitForUser = setTimeout(fail, 10000);
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(createMap, fail);
+  }else {
+    fail();
+  }
+}
+
+function createMap(position) {
+  clearTimeout(waitForUser);
+  var Lat = position.coords.latitude;
+  var Lng = position.coords.longitude;
+  var mapOptions = {
+    center: new google.maps.LatLng(Lat, Lng),
+    zoom: 10
+  };
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+} 
+
+function fail() {
+  document.getElementById('map').innerHTML = 'Unable to access your current location';
+}
