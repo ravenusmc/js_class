aff5273c6ebfc5ccb47816a32e4767ed9a2e94bf
ravenusmc <mcuddy77@gmail.com>
2017-10-27 11:06:59 -0400

    minor changes

--- 1520/midterm/index.js
+++ 1520/midterm/index.js
@@ -1,15 +1,36 @@
-//This file will hold all of the javascript code for the midterm project. 
+/*
+This file will hold all of the javascript code for the midterm project. 
+
+Author: Mike Cuddy
+JS 1520 Midterm Project
+
+Please note that on the HTML I was using the type=number but decided to pull it out. 
+Although keeping it may have made this easier!
+
+*/
+
 
 //Global Variables 
 numbers = []; //This array will hold the numbers. 
 
+
 //This funcion will ensure that the users enter a number. 
 function validation(value, numbers, num){
-  if (isNaN(value)){
+  if (value == ""){
+    alert('Please enter a number on line ' + num);
+  }else if (isNaN(value)){
     alert('Please enter a number on line ' + num);
   }else {
+    value = Number(value);
     numbers.push(value);
   }
+}
+
+//This function will ensure that the array has exactly 5 elements in it. 
+function arrayShort(){
+  alert('You need to enter in five numbers!');
+  //Here I clear the array so that it does not contain numbers everytime the submit form is hit. 
+  numbers = [];
 }
 
 //This function is what will get the numbers from the input, add them together and then 
@@ -17,35 +38,48 @@ function validation(value, numbers, num){
 function addNumbers() {
 
   //Here I am getting the numbers from the user input and placing each number into an array.
-  one = Number(document.getElementById('num_one').value);
+  first = document.getElementById('num_one').value;
   num = 1;
-  validation(one, numbers, num);
-  two = Number(document.getElementById('num_two').value);
+  validation(first, numbers, num);
+  second = document.getElementById('num_two').value;
   num = 2;
-  validation(two, numbers, num);
-  three = Number(document.getElementById('num_three').value);
+  validation(second, numbers, num);
+  third = document.getElementById('num_three').value;
   num = 3;
-  validation(three, numbers, num);
-  four = Number(document.getElementById('num_four').value);
+  validation(third, numbers, num);
+  fourth = document.getElementById('num_four').value;
   num = 4;
-  validation(four, numbers, num);
-  five = Number(document.getElementById('num_five').value);
+  validation(fourth, numbers, num);
+  fifth = document.getElementById('num_five').value;
   num = 5;
-  validation(five, numbers, num);
+  validation(fifth, numbers, num);
 
-  //This variable will hold the total of all the elements in the array. 
-  var total = 0;
+  if (numbers.length != 5){
+    arrayShort()
+  }else {
 
-  //Using a for loop to sum up the numbers in the array and getting the total
-  for (var i = 0; i < numbers.length; i++){
-    total = numbers[i] + total;
+    //This variable will hold the total of all the elements in the array. 
+    var total = 0;
+    //Using a for loop to sum up the numbers in the array and getting the total
+    for (var i = 0; i < numbers.length; i++){
+      total = numbers[i] + total;
+    }
+
+    //Getting the id for where the total will be displayed.
+    answer = document.getElementById('answer')
+    //Setting the total to the innerHTML
+    answer.innerHTML = total;
+
+    //These lines will simply display the numbers entered to the screen
+    document.getElementById('one').innerHTML = first;
+    document.getElementById('two').innerHTML = second;
+    document.getElementById('three').innerHTML = third;
+    document.getElementById('four').innerHTML = fourth;
+    document.getElementById('five').innerHTML = fifth;
+
+    //I use the reset method to set the form back to blank values.
+    document.getElementById("form").reset();
   }
-
-  //Getting the id for where the total will be displayed.
-  answer = document.getElementById('answer')
-  //Setting the total to the innerHTML
-  answer.innerHTML = total;
-
 }
 
 
